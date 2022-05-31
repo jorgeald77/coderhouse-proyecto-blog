ab6b1aece10223dd7a9f6cdecebdec81f31f4462
Jorge G <jorgeald77@gmail.com>
2022-05-30 21:26:24 -0500

    Se crearon los modelos y migraciones de la App blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Home Blog
+def index(request):
+    return HttpResponse("Home Blog")
+
+
+# Posts: List, Show, Create, Edit, Delete
+def post_list(request):
+    return HttpResponse("List Posts")
+
+
+def post_create(request):
+    return HttpResponse("Create Post")
+
+
+def post_show(request):
+    return HttpResponse("Show Post")
+
+
+def post_edit(request):
+    return HttpResponse("Edit Post")
+
+
+def post_delete(request):
+    return HttpResponse("Delete Post")
+
+
+# Comments: List, Show, Create, Edit, Delete
+def comment_list(request):
+    return HttpResponse("List Comments")
+
+
+def comment_create(request):
+    return HttpResponse("Create Comment")
+
+
+def comment_show(request):
+    return HttpResponse("Show Comment")
+
+
+def comment_edit(request):
+    return HttpResponse("Edit Comment")
+
+
+def comment_delete(request):
+    return HttpResponse("Delete Comments")
+
+
+# Users: List, Show, Create, Edit, Delete
+def user_list(request):
+    return HttpResponse("List Users")
+
+
+def user_create(request):
+    return HttpResponse("Create User")
+
+
+def user_show(request):
+    return HttpResponse("Show User")
+
+
+def user_edit(request):
+    return HttpResponse("Edit User")
+
+
+def user_delete(request):
+    return HttpResponse("Delete User")
